c038dd4ae61ebe248ab03718bc96cfef7dba726d
SimoneChiodo <simochi.005@gmail.com>
2025-05-05 18:27:48 +0200

    Optimize code

--- src/main/java/org/lessons/java/spring_la_mia_pizzeria_crud/security/SecurityConfiguration.java
+++ src/main/java/org/lessons/java/spring_la_mia_pizzeria_crud/security/SecurityConfiguration.java
@@ -1,13 +1,11 @@
 package org.lessons.java.spring_la_mia_pizzeria_crud.security;
 
-import org.lessons.java.spring_la_mia_pizzeria_crud.repository.UserRepository;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
